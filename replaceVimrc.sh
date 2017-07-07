f0daf9ba15d43a3dbd79a699e4a579db58be249e
Jackson McNeill <aguyshat@gmail.com>
2017-07-06 21:46:49 -0500

    added dot to vimrc

--- replaceVimrc.sh
+++ replaceVimrc.sh
@@ -2,6 +2,6 @@
 cd
 mv .vimrc .oldvimrc
 git clone https://www.github.com/ai221/vimrc TEMPDIR_DELETE
-mv TEMPDIR_DELETE/.vimrc .
+mv TEMPDIR_DELETE/.vimrc ./.vimrc
 rm TEMPDIR_DELETE -rf
 echo "Vimrc installed, old vimrc renamed to .oldvimrc"
