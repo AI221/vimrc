8ab67c6344945760b0b19af043aced7d070654ce
Jackson McNeill <aguyshat@gmail.com>
2017-07-25 18:14:58 -0500

    fetch tetris.vim

--- replaceVimrc.sh
+++ replaceVimrc.sh
@@ -7,4 +7,8 @@ mv .vimrc .oldvimrc
 git clone https://www.github.com/ai221/vimrc TEMPDIR_DELETE
 mv TEMPDIR_DELETE/.vimrc ./.vimrc
 rm TEMPDIR_DELETE -rf
+
+mkdir ~/.vim
+wget http://www.vim.org/scripts/download_script.php?src_id=24952 -O ~/.vim/tetris.vim
+
 echo "Vimrc installed, old vimrc renamed to .oldvimrc"
